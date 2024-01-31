30d947acadc50d938d0f6256730f85dcf4789126
minju <129808014+betaa0528@users.noreply.github.com>
2024-01-31 10:33:19 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1700 ms, Memory: 124840 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -7,21 +7,13 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] table = new int[N+1][N+1];
+        int num = 0;
         int[][] dp = new int[N+1][N+1];
         for (int i = 1; i < N+1; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j < N+1; j++) {
-                table[i][j] = Integer.parseInt(st.nextToken());
-                if (i == 1 && j == 1) {
-                    dp[i][j] = table[i][j];
-                } else if (j == 1) {
-                    dp[i][j] = table[i][j] + dp[i - 1][j];
-                } else if (i == 1) {
-                    dp[i][j] = table[i][j] + dp[i][j - 1];
-                } else {
-                    dp[i][j] = table[i][j] + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
-                }
+                num = Integer.parseInt(st.nextToken());
+                dp[i][j] = num + dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1];
             }
         }
         while(M --> 0) {
@@ -30,12 +22,7 @@ public class Main {
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-
-            if(x1 == x2 && y1==y2) {
-                System.out.println(table[x1][y1]);
-            } else {
-                System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
-            }
+            System.out.println(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]);
         }
     }
 }
