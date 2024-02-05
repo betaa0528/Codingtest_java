1e86db7958c68f9ead479d8617fd4a264e2d8b11
minju <129808014+betaa0528@users.noreply.github.com>
2024-02-05 13:24:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 168 ms, Memory: 17228 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -3,37 +3,26 @@ import java.io.*;
 public class Main{
     static int blue;
     static int white;
-
+    static int[][] paper;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[][] paper = new int[N][N];
+        paper = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        cutPaper(paper, N);
-
+        cutPaper(N, 0, 0);
         System.out.println(white);
         System.out.println(blue);
-
     }
-
-    private static void cutPaper(int[][] paper, int N) {
-        if (N == 1) {
-            if (paper[0][0] == 0) {
-                white++;
-            } else {
-                blue++;
-            }
-            return;
-        }
+    private static void cutPaper(int N,int x,int y) {
         int whiteCnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = x; i < x + N; i++) {
+            for (int j = y; j < y + N; j++) {
                 if (paper[i][j] == 0) {
                     whiteCnt++;
                 }
@@ -44,29 +33,10 @@ public class Main{
         } else if (whiteCnt == 0) {
             blue++;
         } else {
-            cutPaper(cutting(paper, N, 1), N/2);
-            cutPaper(cutting(paper, N, 2), N/2);
-            cutPaper(cutting(paper, N, 3), N/2);
-            cutPaper(cutting(paper, N, 4), N/2);
+            cutPaper(N / 2, x, y);
+            cutPaper(N / 2, x, y + N / 2);
+            cutPaper(N / 2, x + N / 2, y);
+            cutPaper(N / 2, x + N / 2, y + N / 2);
         }
-    }
-
-    private static int[][] cutting(int[][] paper, int N, int order) {
-        int[][] quarter = new int[N / 2][N / 2];
-
-        for (int i = 0; i < N / 2; i++) {
-            for (int j = 0; j < N / 2; j++) {
-                if (order == 1) {
-                    quarter[i][j] = paper[i][j];
-                } else if (order == 2) {
-                    quarter[i][j] = paper[i][j + N / 2];
-                } else if (order == 3) {
-                    quarter[i][j] = paper[i + N / 2][j];
-                } else if (order == 4) {
-                    quarter[i][j] = paper[i + N / 2][j + N / 2];
-                }
-            }
-        }
-        return quarter;
     }
 }
