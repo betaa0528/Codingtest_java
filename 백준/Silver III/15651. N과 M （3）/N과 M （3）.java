a02fccd3a2e7fb854cbc473e97b1e0a0a1c7b149
minju <129808014+betaa0528@users.noreply.github.com>
2024-02-14 22:56:03 +0900

    [Silver III] Title: N과 M (3), Time: 652 ms, Memory: 160972 KB -BaekjoonHub

---
+++ 백준/Silver III/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+      static boolean[] visit;
+    static int[] arr;
+    static BufferedWriter bw;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+//        st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        visit = new boolean[N];
+        arr = new int[M];
+        dfs(N, M, 0);
+        bw.flush();
+        bw.close();
+    }
+
+    private static void dfs(int N, int M, int depth) throws IOException {
+        if (depth == M) {
+            for (int num : arr) {
+                bw.write(num + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+
+
+            visit[i] = true;
+            arr[depth] = i + 1;
+            dfs(N, M, depth + 1);
+
+            visit[i] = false;
+
+        }
+    }
+}
