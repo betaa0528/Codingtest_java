2bebbf4d57fc5a486e35ca8791756635ed31615f
minju <129808014+betaa0528@users.noreply.github.com>
2024-06-01 10:26:06 +0900

    [Bronze II] Title: 대표값2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,13 +1,4 @@
-a = []
-for test_case in range(5):
-    a.append(int(input()))
-
-for i in range(4):
-    for j in range(5):
-        if a[j]>a[i+1]:
-            temp = a[i+1]
-            a[i+1] = a[j]
-            a[j] = temp
-
-print(sum(a)//5)
+a = [int(input()) for i in range(5)]
+a.sort()
+print(sum(a) // 5)
 print(a[2])
