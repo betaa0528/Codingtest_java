cf1938a531a354c230e26358df4b7459d6330482
minju <129808014+betaa0528@users.noreply.github.com>
2024-06-25 09:48:13 +0900

    [level 1] Title: 옹알이 (2), Time: 0.98 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        for(String babble : babbling) {
-            while(true) {
-                babble = babble.replaceFirst("aya", "1");
-                babble = babble.replaceFirst("ma", "2");
-                babble = babble.replaceFirst("woo", "3");
-                babble = babble.replaceFirst("ye", "4");
-                if(babble.contains("1aya") || babble.contains("2ma") || babble.contains("3woo") || babble.contains("4ye")) {
-                    break;
-                }
-                if(!babble.contains("aya") && !babble.contains("ma") && !babble.contains("woo") && !babble.contains("ye")) {
-                    babble = babble.replace("1" , "");
-                    babble = babble.replace("2" , "");
-                    babble = babble.replace("3" , "");
-                    babble = babble.replace("4" , "");
-                    if(babble.isEmpty()) {
-                        answer++;
-                    }
-                    break;
-                }
+        for (String babble : babbling) {
+            if (babble.contains("ayaaya") || babble.contains("mama") || babble.contains("woowoo") || babble.contains("yeye")) {
+                continue;
+            }
+            babble = babble.replace("aya", "0");
+            babble = babble.replace("ma", "0");
+            babble = babble.replace("woo", "0");
+            babble = babble.replace("ye", "0");
+            babble = babble.replace("0", "");
+            if (babble.isEmpty()) {
+                answer++;
             }
         }
         return answer;
