cabf478200ccdb37afc9e1b2f2340ac44d6f9817
minju <129808014+betaa0528@users.noreply.github.com>
2024-07-09 13:04:24 +0900

    [Gold IV] Title: 플로이드, Time: 156 ms, Memory: 111688 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -8,10 +8,10 @@ for _ in range(bus):
     graph[s][e] = cost if graph[s][e] == 0 else min(graph[s][e], cost)
 
 for i in range(1, city + 1):
+    graph[i][i] = 0
     for j in range(1, city + 1):
         for k in range(1, city + 1):
-            if i != j and j != k and graph[i][k] != 0 and graph[j][i] != 0:
-                graph[j][k] = min(graph[j][i] + graph[i][k], graph[j][k])
+            graph[j][k] = min(graph[j][i] + graph[i][k], graph[j][k])
 
 for i in range(1, city + 1):
     for j in range(1, city + 1):
