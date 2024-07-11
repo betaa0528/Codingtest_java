7526f07813d41d4e77a83f6309575e82d8981f9a
minju <129808014+betaa0528@users.noreply.github.com>
2024-07-11 09:27:01 +0900

    [Silver III] Title: N과 M (6), Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+arr = [0] * m
+def func(cnt, s):
+    if cnt == m:
+        [print(i, end=' ') for i in arr]
+        print()
+        return
+
+    for i in range(s, n):
+        arr[cnt] = nums[i]
+        func(cnt+1, i+1)
+
+func(0,0)
