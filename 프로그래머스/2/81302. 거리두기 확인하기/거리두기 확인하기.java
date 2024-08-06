d0f61354de4e0424ef6c08a5adce2104155e8567
minju <129808014+betaa0528@users.noreply.github.com>
2024-08-06 13:04:21 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.18 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,65 +1,62 @@
 import java.util.*;
 class Solution {
-    public static int[][] coordinate = {
-        {-1, 0}, {-2, 0}, {0, -1}, {0, -2},
-        {1, 0}, {2, 0}, {0, 1}, {0, 2},
-        {-1, 1}, {-1, -1,}, {1, -1}, {1, 1}
-    };
+    public static int[] dx = {0, -1, 0, 1};
+    public static int[] dy = {1, 0, -1, 0};
     public int[] solution(String[][] places) {
-        int[] answer = new int[places.length];
-        Arrays.fill(answer, 1);
-        int index = 0;
-        for(String[] place : places) {
-            List<int[]> list = findPerson(place);
-            for(int[] person: list) {
-                if(!manhattan(place, person)) {
-                    answer[index] = 0;
+        int[] answer = new int[5];
+        for (int i = 0; i < 5; i++) {
+            char[][] graph = new char[5][5];
+            List<int[]> pList = new ArrayList<>();
+
+            int[][] map = new int[5][5];
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                    char val = places[i][j].charAt(k);
+                    graph[j][k] = val;
+                    if (val == 'P') {
+                        pList.add(new int[]{j, k});
+                    }
+                }
+            }
+            answer[i] = 1;
+            for (int[] p : pList) {
+                boolean[][] visit = new boolean[5][5];
+                if(!bfs(p, graph, map, visit)) {
+                    answer[i] = 0;
                     break;
                 }
             }
-            index++;
         }
         return answer;
     }
     
-    public static boolean manhattan(String[] place, int[] person) {
-        for(int i = 0; i<12; i++) {
-            int nx = person[0] + coordinate[i][0];
-            int ny = person[1] + coordinate[i][1];
-            if(nx < 0 || ny < 0 || nx >= place.length || ny >= place[0].length() ) {
-                continue;
+    public static boolean bfs(int[] p, char[][] graph, int[][] map, boolean[][] visit) {
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.add(p);
+        visit[p[0]][p[1]] = true;
+        while(!queue.isEmpty()) {
+            int[] coor = queue.poll();
+            int x = coor[0];
+            int y = coor[1];
+            if(map[x][y] == 3) {
+                return true;
             }
-            
-            if(i > 7) {
-                if(place[nx].charAt(person[1]) == 'X' && place[person[0]].charAt(ny) == 'X') {
+            for(int i=0; i<4; i++) {
+                int cur_x = x + dx[i];
+                int cur_y = y + dy[i];
+                if(cur_x < 0 || cur_x >= 5 || cur_y < 0 || cur_y >= 5) {
                     continue;
                 }
-            }
-            
-            if(place[nx].charAt(ny) == 'P') {
-                System.out.println(person[0] + ", " + person[1] +
-                                  "\t" + nx + ", " + ny);
-                return false;
-            }
-            
-            if(coordinate[i][0] == 0 && Math.abs(coordinate[i][1]) == 1  || coordinate[i][1] == 0 && Math.abs(coordinate[i][0]) == 1) {
-                if(place[nx].charAt(ny) == 'X') {
-                    i += 1;
+                if(!visit[cur_x][cur_y] && graph[cur_x][cur_y] != 'X') {
+                    queue.add(new int[] {cur_x, cur_y});
+                    visit[cur_x][cur_y] = true;
+                    map[cur_x][cur_y] = map[x][y] + 1;
+                    if(map[cur_x][cur_y] <= 2 && graph[cur_x][cur_y] == 'P') {
+                        return false;
+                    }
                 }
             }
         }
         return true;
-    }
-    
-    public static List<int[]> findPerson(String[] place) {
-        List<int[]> list = new ArrayList<>();
-        for(int i=0; i<place.length; i++) {
-            for(int j=0; j<place[0].length(); j++) {
-                if(place[i].charAt(j) == 'P') {
-                    list.add(new int[]{i, j});
-                }
-            }
-        }
-        return list;
     }
 }
