53a7a0a389854723a5c802aa6061c5daad9e0eed
minju <129808014+betaa0528@users.noreply.github.com>
2024-08-20 09:24:25 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14908 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,29 +1,25 @@
 import java.util.*;
 import java.io.*;
-public class Main {
-    static int[] dp;
-
+class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] wines = new int[N];
-        dp = new int[N+1];
-        for(int i=0; i<N; i++) {
-            wines[i] = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[] wines = new int[n+1];
+        int[] dp = new int[n+1];
+        for(int i=0; i<n; i++) {
+            wines[i+1] = Integer.parseInt(br.readLine());
         }
-        if (N == 1) {
-            System.out.println(wines[0]);
-        } else {
-            dp[0] = 0;
-            dp[1] = wines[0];
-            dp[2] = wines[0] + wines[1];
-            for (int j = 3; j < dp.length; j++) {
-                dp[j] = Math.max(dp[j - 1], Math.max(dp[j - 2], dp[j - 3] + wines[j - 2]) + wines[j - 1]);
-            }
-
-            System.out.println(dp[N]);
+        dp[0] = 0;
+        if(n == 1) {
+            System.out.println(wines[1]);
+            return ;
         }
-
+        dp[1] = wines[1];
+        dp[2] = wines[1] + wines[2];
+        for(int i=3; i<=n; i++) {
+            dp[i] = Math.max(Math.max(dp[i-2],  wines[i-1] + dp[i-3]) + wines[i], dp[i-1]) ;
+        }
+        System.out.println(dp[n]);
     }
-
 }
