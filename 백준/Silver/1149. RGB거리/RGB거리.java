727eb7f0ec110428940b680b7e991155a25b3802
minju <129808014+betaa0528@users.noreply.github.com>
2024-08-30 09:31:14 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,36 +1,34 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] dp;
-    static int[][] houses;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = Integer.parseInt(br.readLine());
-        houses = new int[T][3];
-        dp = new int[T][3];
-        int len = houses.length;
-        while (T-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            while (st.hasMoreElements()) {
-                houses[len - T - 1][0] = Integer.parseInt(st.nextToken());
-                houses[len - T - 1][1] = Integer.parseInt(st.nextToken());
-                houses[len - T - 1][2] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[][] houses = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                houses[i][0] = Integer.parseInt(st.nextToken());
+                houses[i][1] = Integer.parseInt(st.nextToken());
+                houses[i][2] = Integer.parseInt(st.nextToken());
             }
         }
+        int[][] dp = new int[n][3];
         dp[0][0] = houses[0][0];
         dp[0][1] = houses[0][1];
         dp[0][2] = houses[0][2];
-        for (int i = 1; i < len; i++) {
-            dp[i][0] = houses[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
-            dp[i][1] = houses[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
-            dp[i][2] = houses[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+        dp[1][0] = Math.min(dp[0][1], dp[0][2]) + houses[1][0];
+        dp[1][1] = Math.min(dp[0][0], dp[0][2]) + houses[1][1];
+        dp[1][2] = Math.min(dp[0][0], dp[0][1]) + houses[1][2];
+        for(int i=2; i<n; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + houses[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + houses[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + houses[i][2];
         }
-        int answer = Math.min(dp[len - 1][0], Math.min(dp[len - 1][1], dp[len - 1][2]));
-        System.out.println(answer);
-    }
 
-   
+        System.out.println(Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]));
+    }
 }
