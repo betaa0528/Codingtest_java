9e3bc7739a62ee9a9f44aef9834672ab48bd0f3e
minju <129808014+betaa0528@users.noreply.github.com>
2025-02-23 14:25:51 +0900

    [level 2] Title: 스킬트리, Time: 9.44 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,25 +2,10 @@ import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        Map<Character, Integer> map = new HashMap<>();
         
-        for(int i=0; i<skill.length(); i++) {
-            map.put(skill.charAt(i), i);
-        }
         for(String sk : skill_trees) {
-            int cnt = 0;
-            boolean flag = true;
-            for(int i=0; i<sk.length(); i++) {
-                if(!map.containsKey(sk.charAt(i))) {
-                    continue;
-                }
-                if(map.get(sk.charAt(i)) != cnt) {
-                    flag = false;
-                    break;
-                }
-                cnt++;
-            }
-            if(flag) {
+            String s = sk.replaceAll("[^" + skill + "]", "");
+            if(skill.startsWith(s) || s.equals(skill)) {
                 answer++;
             }
         }
