f6e0bb10e99ccdfc1b3fc76a7916b27295636276
minju <129808014+betaa0528@users.noreply.github.com>
2025-02-24 13:27:41 +0900

    [level 2] Title: 택배상자, Time: 37.20 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        int cnt = 1;
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0; i<order.length; i++) {
+            
+            for(; cnt<=order[i]; cnt++) {
+                stack.push(cnt);
+            }
+            if(cnt + 1 == order[i]) {
+                answer++;
+                cnt++;
+                continue;
+            }
+            if(!stack.isEmpty() && stack.peek() == order[i]) {
+                answer++;
+                stack.pop();
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
