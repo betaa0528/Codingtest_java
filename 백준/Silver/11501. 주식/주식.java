42ab6c1dd9323bf38646bebf47eb48bbc9fb9cd3
minju <129808014+betaa0528@users.noreply.github.com>
2025-03-20 13:39:28 +0900

    [Silver II] Title: 주식, Time: 1040 ms, Memory: 317296 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -4,30 +4,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++) {
-            int len = Integer.parseInt(br.readLine());
-            int[] stocks = new int[len];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=0; j<len; j++) {
-                stocks[j] = Integer.parseInt(st.nextToken());
+        StringTokenizer st;
+//		= new StringTokenizer(br.readLine(), " ");
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i<t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int data[] = new int[n];
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j<n; j++) {
+                data[j] = Integer.parseInt(st.nextToken());
             }
-            long money = 0;
-            int cnt = 0;
-            // Deque<Integer> q = new ArrayDeque<>();
-            int now = stocks[len-1];
-            for(int k=len-2; k>=0; k--) {
-                if(stocks[k] < now) {
-                    cnt++;
-                    money -= stocks[k];
-                } else {
-                    money += cnt * now;
-                    now = stocks[k];
-                    cnt = 0;
+            int max = data[n-1];
+            long sum = 0;
+            for(int k = n-1; k>=0; k--) {
+                if(data[k]<=max) {
+                    sum += max- data[k];
+                }else {
+                    max = data[k];
                 }
             }
-            money += cnt * now;
-            System.out.println(money);
+            System.out.println(sum);
+        
         }
     }
 }
