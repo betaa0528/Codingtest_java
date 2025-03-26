d5c7c0fa08294b47ba9570da9519ef1553be805f
minju <129808014+betaa0528@users.noreply.github.com>
2025-03-26 14:03:16 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 17.70 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -5,37 +5,24 @@ class Solution {
         int answer = 0;
         
         Map<String,Integer> map = new HashMap<>();
-        Map<Integer, String> name = new HashMap<>();
         int len = friends.length;
+        
         int[][] board = new int[len][len];
+        int[] index = new int[len];
+        
         int idx = 0;
         for(String friend : friends) {
             map.put(friend, idx++);
-            name.put(idx - 1, friend);
         }
         
         for(String gift : gifts) {
             String[] split = gift.split(" ");
-            int sent = map.get(split[0]);
-            int reciever = map.get(split[1]);
-            board[sent][reciever]++;
+            
+            index[map.get(split[0])]++;
+            index[map.get(split[1])]--;
+            board[map.get(split[0])][map.get(split[1])]++;
         }
-        int[][] index = new int[len][2];
-        for(int i=0; i<len; i++) {
-            int sum = 0;
-            for(int j=0; j<len; j++) {
-                sum += board[i][j];
-            }
-            index[i][0] = sum;
-        }
-        
-        for(int j=0; j<len; j++) {
-            int sum = 0;
-            for(int i=0; i<len; i++) {
-                sum += board[i][j];
-            }
-            index[j][1] = sum;
-        }
+
         
         for(int i=0; i<len; i++) {
             int add = 0;
@@ -45,9 +32,8 @@ class Solution {
                     add++;
                 }
                 if(board[i][j] == board[j][i] || board[i][j] == 0 && board[j][i] == 0) {
-                    int sPoint = index[i][0] - index[i][1];
-                    int rPoint = index[j][0] - index[j][1];
-                    if(sPoint > rPoint) {
+                    
+                    if(index[i] > index[j]) {
                         add++;
                     }
                 }
